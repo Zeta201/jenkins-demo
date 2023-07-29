4f22ca5becd1c0f41ca8a4a2ce4eb4ec2757f533
Zeta201 <dini2040gh@gmail.com>
2023-07-29 20:02:00 +0530

    Initial commit

---
+++ src/main/java/com/zeta/springboot/jenkinsdemo/JenkinsDemoApplication.java
@@ -0,0 +1,28 @@
+package com.zeta.springboot.jenkinsdemo;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import jakarta.annotation.PostConstruct;
+
+
+@SpringBootApplication
+public class JenkinsDemoApplication {
+
+	public static Logger logger = LoggerFactory.getLogger(JenkinsDemoApplication.class);
+	
+	@PostConstruct
+	public void init() {
+		logger.info("Application started...");
+	}
+	
+	public static void main(String[] args) {
+		logger.info("Application finished");
+		
+		SpringApplication.run(JenkinsDemoApplication.class, args);
+	}
+
+}
